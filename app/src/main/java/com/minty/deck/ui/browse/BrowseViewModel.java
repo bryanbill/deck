19e06eaaa7a8d2b6f328a7959f0f0e670ff49329
bryanbill <emobrya@gmail.com>
2022-05-29 12:16:00 +0300

    ft:VM - Browse ViewModel

---
+++ app/src/main/java/com/minty/deck/ui/browse/BrowseViewModel.java
@@ -0,0 +1,35 @@
+package com.minty.deck.ui.browse;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.minty.deck.models.HashtagModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrowseViewModel extends ViewModel {
+
+    private final MutableLiveData<List<HashtagModel>> mHashtag;
+    private final List<HashtagModel> mHashtagList = new ArrayList<>();
+
+    public BrowseViewModel() {
+        mHashtag = new MutableLiveData<>();
+        mHashtagList.add(new HashtagModel("#android", 1));
+        mHashtagList.add(new HashtagModel("#java", 1));
+        mHashtagList.add(new HashtagModel("#kotlin", 1));
+        mHashtagList.add(new HashtagModel("#c", 1));
+        mHashtagList.add(new HashtagModel("#c++", 1));
+        mHashtagList.add(new HashtagModel("#c#", 1));
+        mHashtagList.add(new HashtagModel("#php", 1));
+        mHashtagList.add(new HashtagModel("#python", 1));
+        mHashtagList.add(new HashtagModel("+", 1));
+
+        mHashtag.setValue(mHashtagList);
+    }
+
+    public LiveData<List<HashtagModel>> getHashtag() {
+        return mHashtag;
+    }
+}
