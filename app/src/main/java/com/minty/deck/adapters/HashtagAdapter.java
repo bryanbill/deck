1a364fc5778d4804c53497a801b2d2c3472742f4
bryanbill <emobrya@gmail.com>
2022-05-29 12:13:53 +0300

    ft:Adapter - Implements adapter for handling hashtags

---
+++ app/src/main/java/com/minty/deck/adapters/HashtagAdapter.java
@@ -0,0 +1,93 @@
+package com.minty.deck.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.minty.deck.R;
+import com.minty.deck.models.HashtagModel;
+import com.minty.deck.ui.browse.BrowseFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashtagAdapter extends RecyclerView.Adapter<HashtagAdapter.ViewHolder> {
+
+    private Context context;
+    private List<HashtagModel> mHashtagList;
+
+    public HashtagAdapter(Context context, List<HashtagModel> hashtagList) {
+        this.context = context;
+        this.mHashtagList = hashtagList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.hashtag_item, parent, false);
+        return new HashtagAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        HashtagModel hashtagModel = mHashtagList.get(position);
+        holder.hashtag.setText(hashtagModel.getHashtag());
+        if (position == mHashtagList.size() - 1) {
+            HashtagModel lastHashtag = mHashtagList.get(position);
+
+
+            holder.hashtag.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if((ViewGroup)holder.hashTagView.getParent() != null) {
+                        ((ViewGroup)holder.hashTagView.getParent()).removeView(holder.hashTagView);
+                    }
+                    EditText editText = (EditText) holder.hashTagView
+                            .findViewById(R.id.input_hashtag);
+                    new MaterialAlertDialogBuilder(context).setView(holder.hashTagView)
+                            .setMessage("Enter a new hashtag")
+                            .setTitle("Hashtag")
+                            .setNegativeButton("Cancel", (dialog, which) -> {
+                                dialog.dismiss();
+                            })
+                            .setPositiveButton("Add", (dialog, which) -> {
+                                //Add the new hashtag
+                                mHashtagList.remove(0);
+                                mHashtagList.remove(mHashtagList.size() - 1);
+                                String newHashtag = editText.getText().toString();
+                                mHashtagList.add(new HashtagModel(newHashtag, 0));
+                                mHashtagList.add(lastHashtag);
+                                notifyDataSetChanged();
+
+                            })
+                            .show();
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mHashtagList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView hashtag;
+        private View hashTagView;
+
+        public ViewHolder(android.view.View itemView) {
+            super(itemView);
+            hashtag = itemView.findViewById(R.id.hashtag);
+            LayoutInflater layoutInflater = LayoutInflater.from(context);
+            hashTagView = layoutInflater.inflate(R.layout.hashtag_input,
+                    (ViewGroup) itemView.getParent(), false);
+        }
+    }
+}
