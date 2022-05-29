5098c56f0d1f86ecbcb0fc2959562eeec240e861
bryanbill <emobrya@gmail.com>
2022-05-29 12:14:13 +0300

    ft:Adapater - Implements Stories Adapter

---
+++ app/src/main/java/com/minty/deck/adapters/StoryAdapter.java
@@ -0,0 +1,57 @@
+package com.minty.deck.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.minty.deck.R;
+import com.minty.deck.models.UserModel;
+
+import java.util.List;
+
+public class StoryAdapter extends RecyclerView.Adapter<StoryAdapter.ViewHolder> {
+    private Context context;
+    private List<UserModel> storyList;
+
+    public StoryAdapter(Context context, List<UserModel> storyList) {
+        this.context = context;
+        this.storyList = storyList;
+    }
+
+    @NonNull
+    @Override
+    public StoryAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.story_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StoryAdapter.ViewHolder holder, int position) {
+        Glide.with(context).load(storyList.get(position).getProfileImageUrl()).fitCenter().into(holder.imageView);
+        holder.textView.setText(storyList.get(position).getUserName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return storyList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageView;
+        private TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+
+            super(itemView);
+            imageView = itemView.findViewById(R.id.storyImage);
+            textView = itemView.findViewById(R.id.storyTitle);
+        }
+    }
+}
